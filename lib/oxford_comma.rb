a05bbbc04430bf92a2d55f50cb03b4e8b73803f8
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-18 17:18:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join(" ,")
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size == 3
+    word = array.last
+    array.pop #remove the last word from phrase
+    puts array.join(", ") + ", and " + word # add the word to the end
+  elsif array.size > 3
+    word = array.last
+    array.pop #remove the last word from phrase
+    puts array.join(", ") + ", and " + word # add the word to the end
+  end
 end
